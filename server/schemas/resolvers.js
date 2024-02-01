2024c491935330cd9642d8e35c0c144cf55782d3
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2024-01-31 22:16:52 -0500

    final fix and commenting

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,73 +1,112 @@
-const { User, Book } = require("../models");
-const { signToken } = require("../utils/auth")
 
+const { User } = require("../models");
+
+const { signToken } = require("../utils/auth");
+
+// Define resolvers for queries and mutations
 const resolvers = {
   Query: {
+    // Resolver to fetch the current user profile
     me: async (parent, args, context) => {
-      console.log(context.user)
+      
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+        try {
+          
+          const user = await User.findOne({ _id: context.user._id });
+        
+          return user;
+        } catch (error) {
+          
+          throw new Error("Could not find the user");
+        }
       }
-      throw new Error("user not found");
+      // Throw an error if the user is not authenticated
+      throw new Error("User not authenticated");
     },
   },
   Mutation: {
+    // Resolver to handle user login
     login: async (parent, args) => {
-      const user = await User.findOne({ email: args.email });
-      if (!user) {
-        throw new Error("user not found");
-      }
-      const isCorrectPassword = await user.isCorrectPassword(args.password);
-      console.log(!isCorrectPassword);
-      if (!isCorrectPassword) {
+      // Extract email and password from the arguments
+      const { email, password } = args;
+      try {
+       
+        const user = await User.findOne({ email });
+       
+        if (!user) {
+          throw new Error("User not found");
+        }
         
-        throw new Error("incorrect credentials");
+        const isCorrectPassword = await user.isCorrectPassword(password);
+        
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+        // Generate a token for the user
+        const token = signToken(user);
+    
+        return { token, user };
+      } catch (error) {
+       
+        throw new Error("Login failed");
       }
-      const token = signToken(user);
-      return { token, user };
     },
+    // Resolver to add a new user
     addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-      return { token, user };
+      try {
+      
+        const user = await User.create(args);
+        // Generate a token for the new user
+        const token = signToken(user);
+       
+        return { token, user };
+      } catch (error) {
+        // Throw an error if user creation fails
+        throw new Error("Could not create user");
+      }
     },
+    // Resolver to save a book to a user's profile
     saveBook: async (parent, args, context) => {
       if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          {
-            _id: context.user._id,
-          },
-          {
-            $push: {
-              savedBooks: args.input,
-            },
-          },
-          { new: true }
-        );
-        return updatedUser;
+        try {
+          // Update the user's savedBooks array by adding a new book
+          const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { savedBooks: args.input } },
+            { new: true }
+          );
+          
+          return updatedUser;
+        } catch (error) {
+          
+          throw new Error("Could not save the book");
+        }
       }
-      throw new Error("user not found");
+      // Throw an error if the user is not authenticated
+      throw new Error("User not authenticated");
     },
+    // Resolver to remove a book from a user's profile
     removeBook: async (parent, args, context) => {
       if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          {
-            _id: context.user._id,
-          },
-          {
-            $pull: {
-              savedBooks: {
-                bookId: args.bookId
-              }
-            },
-          },
-          { new: true }
-        );
-        return updatedUser;
+        try {
+          // Update the user's savedBooks array by removing a book
+          const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId: args.bookId } } },
+            { new: true }
+          );
+         
+          return updatedUser;
+        } catch (error) {
+          // Throw an error if removing the book fails
+          throw new Error("Could not remove the book");
+        }
       }
-      throw new Error("user not found");
+      // Throw an error if the user is not authenticated
+      throw new Error("User not authenticated");
     },
   },
 };
+
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,46 +1,47 @@
 const { gql } = require("apollo-server-express");
 
+// Define the GraphQL schema using GraphQL Schema Definition Language (SDL)
 const typeDefs = gql`
   input BookInput {
-    authors: [String]
-    description: String
-    title: String!
-    bookId: String!
-    image: String
-    link: String
+    authors: [String]              # List of authors for a book
+    description: String           # Description of the book
+    title: String!                # Title of the book (required)
+    bookId: String!               # Unique identifier for the book (required)
+    image: String                 # URL for the book cover image
+    link: String                  # URL for more information about the book
   }
 
   type User {
-    _id: ID!
-    username: String!
-    email: String!
-    bookCount: Int
-    savedBooks: [Book]
+    _id: ID!                       # Unique identifier for the user
+    username: String!              # Username of the user
+    email: String!                 # Email address of the user
+    bookCount: Int                 # Number of books saved by the user
+    savedBooks: [Book]             # List of books saved by the user
   }
 
   type Book {
-    bookId: ID!
-    authors: [String]
-    description: String
-    title: String!
-    image: String
-    link: String
+    bookId: ID!                    # Unique identifier for the book
+    authors: [String]              # List of authors for the book
+    description: String            # Description of the book
+    title: String!                 # Title of the book (required)
+    image: String                  # URL for the book cover image
+    link: String                   # URL for more information about the book
   }
 
   type Auth {
-    token: ID!
-    user: User
+    token: ID!                     # JWT token for user authentication
+    user: User                     # User information
   }
 
   type Query {
-    me: User
+    me: User                      # Query to fetch the current user
   }
 
   type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: BookInput!): User
-    removeBook(bookId: String!): User
+    login(email: String!, password: String!): Auth             # Mutation for user login
+    addUser(username: String!, email: String!, password: String!): Auth  # Mutation for adding a new user
+    saveBook(input: BookInput!): User                          # Mutation for saving a book
+    removeBook(bookId: String!): User                          # Mutation for removing a book
   }
 `;
 
